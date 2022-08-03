bdbc2d555492117a9ab28919f904144ce2b0ef76
jtmaston <jtmstn1@gmail.com>
2022-08-03 18:45:20 +0300

    Initial Commit

---
+++ AVRSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AVRSim
+//
+//  Created by Alex ANASTASIU on 8/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
